9c62dc26deefcdd6a57eb693eec403a5c30da052
fjvm09 <fjvm09@gmail.com>
2022-11-11 11:27:36 -0600

    Finished tasks

--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -15,28 +15,35 @@ end
 
 def capitalize(word)
   # capitalize the first letter of the word
+  word.capitalize
 end
 
 def uppercase(string)
   # uppercase all letters in the string
+  string.upcase
 end
 
 def downcase(string)
   # downcase all letters in the string
+  string.downcase
 end
 
 def empty_string(string)
   # return true if the string is empty
+  string == ""
 end
 
 def string_length(string)
   # return the length of the string
+  string.length
 end
 
 def reverse(string)
   # return the same string, with all of its characters reversed
+  string.reverse
 end
 
 def space_remover(string)
   # remove all the spaces in the string using gsub
+  string.gsub(" ", "")
 end
